5397b889378548683bebaf51863bf907fc3737d0
Sumit Deo <93740684+deosu@users.noreply.github.com>
2022-05-27 14:16:01 -0700

    Monit 28785 exponential histogram (#189)
    * Java app to send OTLP metric data
    
    Add a maven based Java application to send OTLP metric data.
    
    * Java app to send OTLP metric data
    
    Add a maven based Java application to send OTLP metric data.
    
    * Code Review
    
    Miscellaneous changes as per PR review.
    
    * Readme file update.
    
    Add a warning about internal packages.

---
+++ java-metric-example/src/main/java/com/vmware/ConfigCache.java
@@ -0,0 +1,42 @@
+package com.vmware;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * @author Sumit Deo (deosu@vmware.com)
+ */
+public class ConfigCache {
+  private final Properties configProp = new Properties();
+  private final static Logger logger = LoggerFactory.getLogger(ConfigCache.class);
+
+  private ConfigCache()
+  {
+    //Private constructor to restrict new instances
+    InputStream in = this.getClass().getClassLoader().getResourceAsStream("config.properties");
+    logger.info("Reading all properties from the file");
+    try {
+      configProp.load(in);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static class LazyHolder
+  {
+    private static final ConfigCache INSTANCE = new ConfigCache();
+  }
+
+  public static ConfigCache getInstance()
+  {
+    return LazyHolder.INSTANCE;
+  }
+
+  public String getProperty(String key){
+    return configProp.getProperty(key);
+  }
+}
---
+++ java-metric-example/src/main/java/com/vmware/OtelService.java
@@ -0,0 +1,127 @@
+package com.vmware;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.metrics.LongHistogram;
+import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
+import io.opentelemetry.sdk.metrics.InstrumentSelector;
+import io.opentelemetry.sdk.metrics.InstrumentType;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.SdkMeterProviderBuilder;
+import io.opentelemetry.sdk.metrics.View;
+import io.opentelemetry.sdk.metrics.export.AggregationTemporalitySelector;
+import io.opentelemetry.sdk.metrics.export.MetricReader;
+import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
+import io.opentelemetry.sdk.metrics.internal.view.ExplicitBucketHistogramAggregation;
+import io.opentelemetry.sdk.metrics.internal.view.ExponentialHistogramAggregation;
+import io.opentelemetry.sdk.resources.Resource;
+
+/**
+ * @author Sumit Deo (deosu@vmware.com)
+ */
+public class OtelService {
+
+  public static final int INTERVAL = 1000;
+  final ConfigCache cache;
+  private final static Logger logger = LoggerFactory.getLogger(OtelService.class);
+  public OtelService() {
+    cache = ConfigCache.getInstance();
+    logger.info("Cached configuration for " + cache.getProperty("description"));
+  }
+
+  private Resource resource() {
+    logger.info("Loading resources.");
+    return Resource.getDefault().merge(Resource
+        .create(Attributes.builder()
+            .put("application", cache.getProperty("application"))
+            .put("service.name", cache.getProperty("service.name"))
+            .build()));
+  }
+
+  public LongHistogram initHistogramMetricRecorder() {
+    AggregationTemporalitySelector temporalitySelector;
+    temporalitySelector = getAggregationTemporalitySelector();
+
+    logger.info("Initializing OtlpGrpcMetricExporter.");
+    OtlpGrpcMetricExporter metricExporter = OtlpGrpcMetricExporter.builder()
+        .setEndpoint(cache.getProperty("otel.exporter.otlp.endpoint"))
+        .setAggregationTemporalitySelector(temporalitySelector)
+        .build();
+
+    logger.info("Initializing PeriodicMetricReader.");
+    MetricReader periodicReader =
+        PeriodicMetricReader.builder(metricExporter).setInterval(Duration.ofMillis(INTERVAL)).build();
+
+    logger.info("Initializing SdkMeterProviderBuilder.");
+    SdkMeterProviderBuilder builder = SdkMeterProvider.builder()
+        .setResource(resource())
+        .registerMetricReader(periodicReader);
+
+    logger.info("Initializing InstrumentSelector with type Histogram.");
+    InstrumentSelector selector =
+        InstrumentSelector.builder().setType(InstrumentType.HISTOGRAM).build();
+
+    View view = createView();
+
+    logger.info("Initializing SdkMeterProvider.");
+    SdkMeterProvider sdkMeterProvider = builder
+        .registerView(selector, view)
+        .build();
+
+    Runtime.getRuntime().addShutdownHook(new Thread(sdkMeterProvider::shutdown));
+
+    logger.info("Initializing Metric Recorder.");
+    return sdkMeterProvider
+        .get(cache.getProperty("instrumentation.library.name"))
+        .histogramBuilder(cache.getProperty("metric.name"))
+        .setDescription(cache.getProperty("description"))
+        .setUnit(cache.getProperty("metric.unit"))
+        .ofLongs()
+        .build();
+  }
+
+  private View createView() {
+    View view;
+    if (cache.getProperty("metric.sub.type").equals("exponential")) {
+      view = View.builder()
+          .setAggregation(ExponentialHistogramAggregation.create(Integer.parseInt(cache.getProperty("scale")),
+              Integer.parseInt(cache.getProperty("max.buckets"))))
+          .build();
+      logger.info("Initializing View with aggregation ExponentialHistogramAggregation. " +
+          "MaximumBucketSize: " + cache.getProperty("max.buckets") + " and Scale: " + cache.getProperty("scale"));
+    } else {
+      String bucketBoundaries = cache.getProperty("metric.bucket.boundaries");
+      List<Double> boundaries = new ArrayList<>();
+
+      for (String str : bucketBoundaries.split(",")) {
+        boundaries.add(Double.valueOf(str));
+      }
+
+      view = View.builder()
+          .setAggregation(ExplicitBucketHistogramAggregation.create(boundaries))
+          .build();
+      logger.info("Initializing View with aggregation ExplicitBucketHistogramAggregation. " +
+          "ExplicitBucketCounts: " + boundaries.size());
+    }
+    return view;
+  }
+
+  private AggregationTemporalitySelector getAggregationTemporalitySelector() {
+    AggregationTemporalitySelector temporalitySelector;
+    if (cache.getProperty("aggregation.temporality").equals("delta")) {
+      temporalitySelector = AggregationTemporalitySelector.deltaPreferred();
+      logger.info("Delta aggregation temporality selected.");
+    }
+    else {
+      temporalitySelector = AggregationTemporalitySelector.alwaysCumulative();
+      logger.info("Cumulative aggregation temporality selected.");
+    }
+    return temporalitySelector;
+  }
+}
